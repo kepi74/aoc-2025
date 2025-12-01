d0cd19015ee2ae09a962609555783f4477fdd1ca
Pavel Kepka <pavel.kepka@ataccama.com>
2025-12-01 10:27:27 +0100

    add readData utility function to read input data files

---
+++ src/utils/read-data.ts
@@ -0,0 +1,18 @@
+import { readFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+
+export function readData({
+  day,
+  example = false,
+}: {
+  day: number
+  example?: boolean
+}): Array<string> {
+  const filePath = resolve(
+    import.meta.dirname,
+    '../data',
+    `day-${String(day).padStart(2, '0')}${example ? '-example' : ''}.txt`,
+  )
+  const fileContent = readFileSync(filePath, 'utf-8')
+  return fileContent.split('\n')
+}
